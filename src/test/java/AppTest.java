8eae61fb629ff8a1872526b75bb9eb300be9b875
Daniël Richtersz <digidi-daantje@hotmail.com>
2018-03-07 14:23:12 +0100

    test 3

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -6,7 +6,7 @@ public class AppTest {
     @Test public void testAppHasGreeting() throws Exception {
         App testApp = new App();
         System.out.print("Start testAppHasGreeting");
-        assertTrue("Values must be the same as the retrieved values", (testApp.getGreeting() == "Application runs, now with automated building on Git push"));
+        assertTrue("Values must be the same as the retrieved values 2", (testApp.getGreeting() == "Application runs, now with automated building on Git push"));
 
     }
 }
